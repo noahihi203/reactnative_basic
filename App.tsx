ce5922573dc8b32bff5a053fc8f4fe3feffcb920
Lâm Bùi Quốc <22520733@ms.uit.edu.vn>
2024-09-18 21:50:46 +0700

    icons

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { Alert, Button, FlatList, Keyboard, Pressable, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
-import FlexBox from './components/flex.box';
-
+import AntDesign from '@expo/vector-icons/AntDesign';
 interface ITodo {
   id: number,
   name: string,
@@ -44,77 +43,94 @@ export default function App() {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   return (
-    // <TouchableWithoutFeedback onPress={()=> Keyboard.dismiss()}>
-    //   <View style={styles.container}>
-    //     <Text style={styles.header}>Todo App</Text>
-    //     {/* form */}
-    //     <View style={styles.body}>
-    //       <TextInput
-    //         value={todo}
-    //         style={styles.todoInput}
-    //         onChangeText={(value) => setTodo(value)}
-    //       />
-    //       <Button
-    //         title='Add todo'
-    //         onPress={handleAddTodo}
-    //       />
-    //     </View>
-    //     {/* list todo */}
-    //     <View style={styles.body}>
-    //       <FlatList
-    //         data={listTodo}
-    //         keyExtractor={item => item.id + ""}
-    //         renderItem={({ item }) => {
-    //           return (
-    //             <Pressable
-    //               style={({ pressed }) => ({ opacity: pressed ? 0.5 : 1 })}
-    //               onPress={() => deleteTodo(item.id)}
-    //             >
-    //               <Text
-    //                 style={styles.todoItem}>{item.name}
-    //               </Text>
-    //             </Pressable>
-    //           )
-    //         }}
-    //       />
-    //     </View>
-    //   </View >
-    // </TouchableWithoutFeedback>
-    <FlexBox/>
-  );
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <Text style={styles.header}>Todo App</Text>
+        {/* form */}
+        <View style={styles.form}>
+          <TextInput
+            value={todo}
+            style={styles.todoInput}
+            onChangeText={(value) => setTodo(value)}
+          />
+          <Button
+            title='Add todo'
+            onPress={handleAddTodo}
+          />
+        </View>
+        {/* list todo */}
+        <View style={styles.todo}>
+          <FlatList
+            data={listTodo}
+            keyExtractor={item => item.id + ""}
+            renderItem={({ item }) => {
+              return (
+                <Pressable
+                  style={({ pressed }) => ({ opacity: pressed ? 0.5 : 1 })}
+                  onPress={() => deleteTodo(item.id)}
+                >
+                  <View style={styles.groupTodo}>
+                    <Text
+                      style={styles.todoItem}>{item.name}
+                    </Text>
+                    <AntDesign name="close" size={24} color="black" />
+                  </View>
+                </Pressable>
+              )
+            }}
+          />
+        </View>
+      </View >
+    </TouchableWithoutFeedback>
+  )
 }
 
 const styles = StyleSheet.create({
-  header: {
-    backgroundColor: "orange",
-    paddingHorizontal: 20,
-    textAlign: "center",
-    fontSize: 40,
+  groupTodo: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderWidth: 1,
+    borderStyle: "dashed",
+    marginBottom: 15,
+    padding: 15,
+    marginHorizontal: 10,
   },
   container: {
     paddingTop: 50,
     flex: 1,
     backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+
+  },
+
+  header: {
+    backgroundColor: "orange",
+    paddingHorizontal: 20,
+    textAlign: "center",
+    fontSize: 40,
+    //flex: 1,
+  },
+
+  form: {
+    //flex: 2,
+    marginBottom: 20,
+  },
+  todo: {
+    flex: 1,
   },
   todoInput: {
     borderBottomWidth: 1,
     borderBottomColor: "green",
-    padding: 15,
+    padding: 5,
     margin: 15,
   },
   todoItem: {
     fontSize: 20,
-    marginBottom: 20,
-    borderWidth: 1,
-    borderStyle: "dashed",
-    padding: 10,
   },
   body: {
     paddingHorizontal: 10,
     marginBottom: 20,
-
+    flex: 1,
   }
 
 });
